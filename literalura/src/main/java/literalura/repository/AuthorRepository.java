306870960ed765269ca698bdac160aeef16f6759
Karen <karenkessia16@gmail.com>
2024-12-06 12:24:35 -0400

    ad archives

---
+++ literalura/src/main/java/literalura/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package literalura.repository;
+
+import com.alura.literalura.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
---
+++ literalura/src/main/java/literalura/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package literalura.repository;
+
+import com.alura.literalura.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
